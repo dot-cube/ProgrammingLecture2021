ada5839f9c2145f396d6481332e8bbecad13e559
hi19051 <hi19051@users.noreply.github.com>
2021-05-26 13:37:39 +0000

    Automated deployment: Wed May 26 13:37:39 UTC 2021 6fc52b6d64893c1e82d6fd9b79394e9e8481258c

--- hi19051/sketch.js
+++ hi19051/sketch.js
@@ -1,34 +1,39 @@
 let leftSketch = function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth/2, p.windowHeight);
+    p.createCanvas(p.windowWidth / 2, p.windowHeight);
     setUpCanvas(p);
-  }
-  p.draw = function () {
-    if (p.mouseIsPressed) {
-      p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
-    }
   }
 };
 
 let rightSketch = function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth/2, p.windowHeight);
-    //p.position(leftSketch.size().width,0);
+    p.createCanvas(p.windowWidth / 2, p.windowHeight);
     setUpCanvas(p);
-  }
-  p.draw = function () {
-    if (p.mouseIsPressed) {
-      p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
-    }
   }
 };
 
 function setUpCanvas(p) {
   p.background(0);
 
-  p.rect(0, 0, p.windowWidth/2, p.windowHeight);
+  p.rect(0, 0, p.windowWidth / 2, p.windowHeight);
 };
 
+function sync(h, c) {
+  h.line(h.pmouseX, h.pmouseY, h.mouseX, h.mouseY);
+  c.line(h.windowWidth / 2 - h.pmouseX, h.pmouseY, h.windowWidth / 2 - h.mouseX, h.mouseY);
+}
 
-new p5(leftSketch, 'left_container');
-new p5(rightSketch, 'right_container');
+
+let l = new p5(leftSketch, 'left_container');
+let r = new p5(rightSketch, 'right_container');
+
+l.draw = function () {
+  if (l.mouseIsPressed) {
+    sync(l, r);
+  }
+}
+r.draw = function () {
+  if (r.mouseIsPressed) {
+    sync(r, l);
+  }
+}
